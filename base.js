79c5e94c4fafade3bbd0c2c9aae8400c2e886046
David Zukowski <zukoweb@gmail.com>
2015-12-11 14:45:34 -0500

    feat(eslint-config-defaults): add package and configure extends
    fix(defaults): use airbnb defaults rather than eslint defaults

--- base.js
+++ base.js
@@ -4,29 +4,10 @@ module.exports = {
     'node': true,
   },
   'extends': [
-    'ta-webapp/rules/best-practices',
-    'ta-webapp/rules/errors',
-    'ta-webapp/rules/es6',
+    'defaults/configurations/airbnb',
     'ta-webapp/rules/jsdoc',
-    'ta-webapp/rules/legacy',
     'ta-webapp/rules/mocha',
-    'ta-webapp/rules/react',
-    'ta-webapp/rules/strict',
-    'ta-webapp/rules/style',
-    'ta-webapp/rules/variables',
   ],
   'parser': 'babel-eslint',
-  'rules': {}
+  'rules': {},
 };
-
-
-
-
-
-
-
-
-
-
-
-
